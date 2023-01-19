bebdf6bca289f199888df07132ddf1e41d905c90
Kazushi Eguchi <sandwhale1010@gmail.com>
2023-01-19 16:26:26 +0900

    task追加中

--- task/models/res_partner.py
+++ task/models/res_partner.py
@@ -2,7 +2,10 @@ from odoo import models, fields, api
 
 
 class rtw_sf_partner_task(models.Model):
-    _inherit = "res.partner"
+    _inherit = [
+        "res.partner",
+        # "task.thread"
+    ]
     _description = 'task'
 
-    rtw_task_ids = fields.One2many('task.task')
+    # rtw_task_ids = fields.One2many('task.task')
--- task/models/task_thread.py
+++ task/models/task_thread.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 
 class TaskThread(models.AbstractModel):
-    _name = 'task.thread'
+    _name = 'task.thread.mixin'
     _description = 'Task Thread'
 
     rtw_task_ids = fields.One2many('task.task' 'res_id', string='Tasks', auto_join=True)
