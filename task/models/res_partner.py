b4dcf8f87a5af2b07ad8783f65606249a5ac1dcf
Kazushi Eguchi <sandwhale1010@gmail.com>
2023-01-20 20:14:53 +0900

    Mixin試行中

--- task/models/res_partner.py
+++ task/models/res_partner.py
@@ -9,9 +9,5 @@ class rtw_sf_partner_task(models.Model):
     _description = 'task'
 
     # rtw_task_ids = fields.One2many('task.task')
-    channel_ids = fields.Many2many('mail.channel',
-                                   'mail_channel_task_thread_mixin_partner',
-                                   'partner_id',
-                                   'channel_id',
-                                   string='Channels', copy=False)
-    meeting_ids = fields.Many2many(relation='mail_channel_task_thread_mixin_partner')
+    channel_ids = fields.Many2many(relation='channel_task_thread_mixin_partner')
+    meeting_ids = fields.Many2many(relation='meeting_task_thread_mixin_partner')
