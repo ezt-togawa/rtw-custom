032ad5370fd0ab85082d3ca0ebdf3f9c08c5d033
Kazushi Eguchi <sandwhale1010@gmail.com>
2023-02-06 20:59:41 +0900

    CRM SR集計

--- rtw_crm/models/crm_lead.py
+++ rtw_crm/models/crm_lead.py
@@ -806,10 +806,11 @@ class rtw_crm(models.Model):
                 # print(rec.id)
                 # res = rec.search([('calendar_ids.situation', '=', "4"),
                 #                    ('id', '=', rec.id)])
-                res = self.env['calendar.event'].search([('opportunity_id', '=', rec.id),
-                                                               ('situation', '=', "4")])
-                # print(res.name)
-                rec.sr_status = min(res).sr.name + str(min(res).start.year)
+                res = self.env['calendar.event'].search([('opportunity_id', '=', rec.id), ('situation', '=', "4")])
+                if res:
+                    rec.sr_status = min(res).sr.name + str(min(res).start.year)
+                else:
+                    rec.sr_status = ""
                 # print(min(res.calendar_ids).sr.name)
                 # for line in res:
                 #     print(line.sr.name)
