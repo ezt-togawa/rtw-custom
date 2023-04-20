0154382487727713f1ccdc9e2b828a5d1e4fe49c
khanh <khanhpzo99@gmail.com>
2023-04-20 18:18:01 +0700

    feedback 1

--- rtw_mrp_custom/models/mrp_production.py
+++ rtw_mrp_custom/models/mrp_production.py
@@ -23,3 +23,20 @@ class rtw_mrp_production_revised_edition(models.Model):
                 'default_owner_id': self.env.user.id,
             }
         }
+
+    def _compute_display_name(self):
+        for record in self:
+            project_name = ''
+            estimated_shipping_date=''
+            if record.origin:
+                production = self.env['mrp.production'].browse(record.id) #GET PRODUCTION
+                sale_order = self.env['sale.order'].search([ #GET SALE_ORDER By order_id
+                    ('name','=', production.origin)
+                ], limit=1)
+                if sale_order and sale_order.title:
+                    project_name =f'/{sale_order.title}'
+                if sale_order and sale_order.estimated_shipping_date:
+                    estimated_shipping_date =f'/{sale_order.estimated_shipping_date}'
+                record.display_name = f'{record.origin}{estimated_shipping_date}{project_name}'
+            else:
+                record.display_name = ''
