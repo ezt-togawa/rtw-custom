ef1d6a64bcdfcc11d8921bad457855f852a5a736
khanh <khanhpzo99@gmail.com>
2023-06-19 15:37:50 +0700

    fix fb 10

--- rtw_mrp_back_to_draft/models/mrp_production.py
+++ rtw_mrp_back_to_draft/models/mrp_production.py
@@ -8,12 +8,20 @@ class rtw_mrp_production_back_to_draft(models.Model):
 
     def back_to_draft(self):
         self.state = 'draft'
+        for move in self.move_raw_ids:
+            move.state = 'draft'
 
     def action_confirm(self):
         for production in self:
             if production.state == 'draft':
                 production.state = 'confirmed'
             return super(rtw_mrp_production_back_to_draft,self).action_confirm()
+
+    def action_cancel(self):
+        for production in self:
+            if production.state == 'draft':
+                production.state = 'cancel'
+            return super(rtw_mrp_production_back_to_draft,self).action_cancel()
 
     @api.depends(
         'move_raw_ids.state', 'move_raw_ids.quantity_done', 'move_finished_ids.state',
