bf59133c6b2e3fa972d3ffa5b8b06ed3e5576248
khanh <khanhpzo99@gmail.com>
2023-08-14 16:30:03 +0700

    report pdf full

--- custom_report_rtw/models/sale_order.py
+++ custom_report_rtw/models/sale_order.py
@@ -8,19 +8,18 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     so_title = fields.Char(string="title")
 
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
 
-# class SaleOrderLine(models.Model):
-#     _inherit = 'sale.order.line'
-#
-#     show_details = fields.Boolean(
-#         string="Show details",
-#         default=True)
-#     show_subtotal = fields.Boolean(
-#         string="Show subtotal",
-#         default=True)
-#
-#     def _prepare_invoice_line(self, qty):
-#         res = super()._prepare_invoice_line(qty)
-#         res.update(show_details=self.show_details,
-#                    show_subtotal=self.show_subtotal)
-#         return res
+    show_details = fields.Boolean(
+        string="Show details",
+        default=True)
+    show_subtotal = fields.Boolean(
+        string="Show subtotal",
+        default=True)
+
+    def _prepare_invoice_line(self, qty):
+        res = super()._prepare_invoice_line(qty)
+        res.update(show_details=self.show_details,
+                   show_subtotal=self.show_subtotal)
+        return res
