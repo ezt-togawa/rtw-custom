a2e1b0d56efcaf1978ba921f3d040be0411470ca
khanh <khanhpzo99@gmail.com>
2023-08-16 11:11:42 +0700

    fix package pdf

--- custom_report_rtw/models/sale_order.py
+++ custom_report_rtw/models/sale_order.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
-
+import math
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -17,6 +17,17 @@ class SaleOrderLine(models.Model):
     show_subtotal = fields.Boolean(
         string="Show subtotal",
         default=True)
+    calculate_packages = fields.Integer(
+        string="Calculate packages",
+        compute="_compute_calculate_packages"
+    )
+
+    def _compute_calculate_packages(self):
+        for line in self:
+            if line.product_id.two_legs_scale:
+                line.calculate_packages = math.ceil(line.product_uom_qty / line.product_id.two_legs_scale)
+            else:
+                line.calculate_packages = line.product_uom_qty
 
     def _prepare_invoice_line(self, qty):
         res = super()._prepare_invoice_line(qty)
