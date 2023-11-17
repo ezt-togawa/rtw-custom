f9d680ad970ed14b888bf51686191f07096c466b
Phu270298 <phunh@axalize.vn>
2023-11-17 15:43:46 +0700

    task no40

--- sale_order_remark/models/sale_order_remark.py
+++ sale_order_remark/models/sale_order_remark.py
@@ -6,12 +6,13 @@ class sale_order_remark(models.Model):
     _inherit = "sale.order"
 
     remark = fields.Text('共通備考', default=None)
+    special_note = fields.Text('伝票用特記事項' , default=None)
 
 class mrp_remark(models.Model):
     _inherit = "mrp.production"
 
     remark = fields.Text('共通備考' , default=None , compute='_compute_remark', inverse='_inverse_remark')
-
+    special_note = fields.Text('伝票用特記事項' , default=None , compute='_compute_special_note', inverse='_inverse_special_note')
 
     def _compute_remark(self):
         for record in self:
@@ -24,3 +25,15 @@ class mrp_remark(models.Model):
             sale_order = self.env['sale.order'].search([('name', '=', record.origin)])
             if sale_order:
                 sale_order.write({'remark': record.remark})
+
+    def _compute_special_note(self):
+        for record in self:
+            sale_order = self.env['sale.order'].search([('name', '=', record.origin)])
+            if sale_order:
+                record.special_note = sale_order.special_note
+
+    def _inverse_special_note(self):
+        for record in self:
+            sale_order = self.env['sale.order'].search([('name', '=', record.origin)])
+            if sale_order:
+                sale_order.write({'special_note': record.special_note})
