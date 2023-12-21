e268f476acfa30bde311e726217585cdc99b028a
HaiPhu2702 <phukaht@gmail.com>
2023-12-21 17:02:04 +0700

    fix translation-task

--- custom_report_rtw/__manifest__.py
+++ custom_report_rtw/__manifest__.py
@@ -26,31 +26,44 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        # quotation
         'report/report_quotation.xml',
+        'report/report_quotation_oversea.xml',
         'report/report_list_price_quotation.xml',
         'report/report_unit_price.xml',
+        'report/purchase_order.xml',
+        'report/report_product_spec.xml',
+        
+        # delivery
         'report/report_delivery_order.xml',
         'report/report_shipping_order.xml',
-        'report/purchase_order.xml',
+        
+        # invoice
+        'report/report_invoice.xml',
+        
+        # purchase order
+        'report/purchase_order_sheet_for_part.xml',
+        
+        # mrp
         'report/purchase_order_sheet.xml',
         'report/inspection_order_form.xml',
-        'report/purchase_order_sheet_for_part.xml',
-        'report/report_invoice.xml',
-        'report/report_product_spec.xml',
-        'report/report_quotation_oversea.xml',
-        'report/stock_picking.xml',
-        'report/purchase.xml',
-        # 'report/spec_list.xml',
+        
+        #general
         'report/external_layout.xml',
         'report/internal_layout.xml',
         'report/header_footer.xml',
-        'report/mrp.xml',
-        'report/invoice.xml',
         'views/views.xml',
         'views/templates.xml',
+        "views/res_company_view.xml",
+        
+        'report/purchase.xml',
+        'report/spec_list.xml',
+        'report/mrp.xml',
+        'report/invoice.xml',
+        
+        # 'report/stock_picking.xml',
         # "views/sale_views.xml",
         # "views/assets.xml",
-        "views/res_company_view.xml"
     ],
     # only loaded in demonstration mode
     'demo': [
--- custom_report_rtw/models/sale_order.py
+++ custom_report_rtw/models/sale_order.py
@@ -8,6 +8,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     so_title = fields.Char(string="title")
     calculate_planned_date = fields.Date(string="planned date" , compute="_compute_calculate_planned_date")
+    so_work_days = fields.Char(string="planned date" , compute="_compute_so_work_days")
 
     def _compute_calculate_planned_date(self):
         max_planned_date = ''
@@ -17,6 +18,21 @@ class SaleOrder(models.Model):
             elif line.date_planned and line.date_planned > max_planned_date:
                 max_planned_date = line.date_planned
         self.calculate_planned_date = max_planned_date
+    def _compute_so_work_days(self):
+        for line in self:
+            if line.lang_code == "en_US":
+                workdays_map = {
+                    "発注後約 4週以内": "About 4 weeks or less after ordering",
+                    "発注後約 5-6週間": "About 5-6 weeks after ordering",
+                    "発注後約 6-7週間": "About 6-7 weeks after ordering",
+                    "発注後約 7-8週間": "About 7-8 weeks after ordering",
+                    "発注後約 8-10週間": "About 8-10 weeks after ordering",
+                    "発注後約 10-12週間": "About 10-12 weeks after ordering",
+                    "発注後約 12以上": "About 12 weeks or more after ordering"
+                }
+                line.so_work_days = workdays_map.get(line.workdays, False)    
+            else:
+                line.so_work_days = line.workdays
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
