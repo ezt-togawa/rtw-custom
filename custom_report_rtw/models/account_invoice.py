9fb0609149d7ad0a6daf0d715296107bf4547f1b
HaiPhu2702 <phukaht@gmail.com>
2024-01-17 11:32:45 +0700

    fix no51

--- custom_report_rtw/models/account_invoice.py
+++ custom_report_rtw/models/account_invoice.py
@@ -17,15 +17,27 @@ class AccountInvoiceLine(models.Model):
 class AccountMoveCus(models.Model):
     _inherit = 'account.move'
 
-    send_to_company = fields.Char(string="send to company" , compute="_compute_send_to")
-    send_to_people = fields.Char(string="send to people" , compute="_compute_send_to")
-    
+    send_to_company = fields.Char(string="send to company", compute="_compute_send_to")
+    send_to_people = fields.Char(string="send to people", compute="_compute_send_to")
+
     def _compute_send_to(self):
-        for l in self:
-            if l.lang_code == 'en_US':
-                l.send_to_people = ('Mr./Mrs. ' + l.partner_id.name) if l.partner_id and l.partner_id.name else ''
-                l.send_to_company = ('御中 ' + l.partner_id.company_id.name  + ' 株式会社') if l.partner_id and l.partner_id.company_id else ''
-            else:
-                l.send_to_people = (l.partner_id.name +' 様') if l.partner_id and l.partner_id.name else ''
-                l.send_to_company = ('株式会社 ' + l.partner_id.company_id.name  + ' 御中') if l.partner_id and l.partner_id.company_id else ''
-                
+        for line in self:
+            partner_name = ''
+            company_name = ''
+            if line.lang_code == 'en_US' and line.partner_id:
+                res_partner = self.env['res.partner'].search([('id', '=', line.partner_id.id)])
+                if res_partner:
+                    for partner in res_partner:
+                        partner_name = ('Mr./Mrs. ' + partner.last_name) if partner.last_name else ''
+                        company_name = ('御中 ' + partner.parent_id.name + ' 株式会社') if partner.parent_id else ''
+            elif line.lang_code != 'en_US' and line.partner_id:
+                res_partner = self.env['res.partner'].search([('id', '=', line.partner_id.id)])
+                if res_partner:
+                    for partner in res_partner:
+                        partner_name = (partner.last_name + ' 様') if partner.last_name else ''
+                        company_name = ('株式会社 ' + partner.parent_id.name + ' 御中') if partner.parent_id else ''
+            
+            if 'send_to_people' in line:
+                line.send_to_people = partner_name
+            if 'send_to_company' in line:
+                line.send_to_company = company_name
