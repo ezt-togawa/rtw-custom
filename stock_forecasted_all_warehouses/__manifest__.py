c85b0c66938cd2c4bb2314c48db166a71c077844
khanh <khanhpzo99@gmail.com>
2024-03-01 16:03:42 +0700

    no 66

---
+++ stock_forecasted_all_warehouses/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "stock_forecasted_all_warehouses",
+    'author': "Enzantrades",
+    'category': 'stock',
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'website': 'http://www.yourcompany.com',
+    'description': """
+""",
+    'version': '14.0.0.0',
+    'depends': ['sale' , 'base', 'stock'],
+    'data': [
+    ],
+    'qweb': [
+        'static/src/xml/stock_forecasted_all_warehouses.xml',
+    ],
+}
