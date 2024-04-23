8a8ff604644a3af37ffc5eec37aa3a1ee23ec649
Phu270298 <phunh@axalize.vn>
2024-04-23 09:01:35 +0700

    fix rtw_mailing_trace_add_contact

--- rtw_mailing_trace_add_contact/models/mailing_trace.py
+++ rtw_mailing_trace_add_contact/models/mailing_trace.py
@@ -6,16 +6,8 @@ class rtw_sf_partner(models.Model):
     
     name_contact = fields.Char(string="Contact", compute="_compute_contact", store=True)
     company_info = fields.Char(string="Company Info", compute="_compute_contact", store=True)    
-    res_partner = fields.Many2one('res.partner', 'Related Partner', compute="_compute_res_partner")
     
-    def _compute_res_partner(self):
-        partner = False
-        for trace in self:
-            if trace.model == 'res.partner' and trace.res_id:
-                partner = self.env['res.partner'].with_context({'lang':self.env.user.lang  or 'en_US'}).search([('id', '=', trace.res_id)], limit=1)
-        trace.res_partner = partner 
-    
-    @api.depends('res_partner','res_partner.last_name', 'res_partner.first_name', 'res_partner.company_type', 'res_partner.name') 
+    @api.depends('res_id')
     def _compute_contact(self):
         for ml in self:
             name_contact = ''
@@ -32,8 +24,8 @@ class rtw_sf_partner(models.Model):
                             company_info = res_partner.parent_id.name  
                     elif res_partner.company_type == 'company':
                         name_contact = res_partner.name
-            ml.name_contact = name_contact
-            ml.company_info = company_info
+            ml.name_contact = name_contact.rstrip()
+            ml.company_info = company_info.rstrip()
 
     def open_res_partner_form_view(self):
         self.ensure_one()
