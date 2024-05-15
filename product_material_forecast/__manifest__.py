513b03ed4d39f37c97d87236ddb020b832ef4ac9
khanh <khanhpzo99@gmail.com>
2024-05-15 16:57:37 +0700

    no 105

--- product_material_forecast/__manifest__.py
+++ product_material_forecast/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale_management', 'purchase', 'stock', 'mrp'],
+    'depends': ['base', 'sale_management', 'purchase', 'stock', 'mrp','sale_order_line_outlook_stock'],
 
     # always loaded
     'data': [
