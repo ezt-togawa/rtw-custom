daccdb52c7bda817605657c5c093306e925563aa
khanh <khanhpzo99@gmail.com>
2024-05-17 11:13:59 +0700

    fix no94 (revert old module and code in sale_order_line_reorder_fields)

--- sale_order_line_reorder_fields/__manifest__.py
+++ sale_order_line_reorder_fields/__manifest__.py
@@ -21,7 +21,7 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale_management', 'sale_sourced_by_line', 'stock' , 'sale' , 'sale_order_rtw' , 'product_spec_rtw', 'sale_order_line_planned_date' , 'sale_order_line_product_pack' ,
-                'sale_order_line_outlook_stock','sale_order_line_delivery_confirmation','sale_order_line_warehouse','sale_line_bom_stock','sale_stock','product_configurator_sale'],
+                'sale_order_line_outlook_stock','sale_order_line_delivery_confirmation','sale_order_line_warehouse','sale_line_bom_stock','sale_stock','product_configurator_sale','rtw_stock_move_line'],
 
     # always loaded
     'data': [
