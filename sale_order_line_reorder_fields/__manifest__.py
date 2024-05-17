f36ace9019edc8c23347d3e0ad67b44c3d96e3d2
小川智丈 <t_ogawa@enzantrades.co.jp>
2024-05-17 18:27:36 +0900

    manifestの不備修正＆項目一部変更

--- sale_order_line_reorder_fields/__manifest__.py
+++ sale_order_line_reorder_fields/__manifest__.py
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/sale_order.xml',
+        'views/sale_order_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
