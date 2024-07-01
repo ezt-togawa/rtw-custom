3084d437d9380bf320f04fbd42692d6f070991b4
Phu270298 <phunh@axalize.vn>
2024-07-01 16:45:45 +0700

    task 129 131

--- rtw_mrp_custom/models/mrp_production.py
+++ rtw_mrp_custom/models/mrp_production.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
+from dateutil.parser import parse
+from datetime import datetime
+import pytz
 
-
-class rtw_mrp_production_revised_edition(models.Model):
+class MrpProductionCus(models.Model):
     _inherit = 'mrp.production'
 
     revised_edition_ids = fields.One2many(
         comodel_name="mrp.revised_edition",
         inverse_name="mrp_id",
         string="revised edition")
-
+    prod_parts_arrival_schedule = fields.Char(string="製造部材入荷予定")
+    is_drag_drop_calendar = fields.Boolean()
+    
     def create_revised_edition(self):
         return {
             'type': 'ir.actions.act_window',
@@ -27,32 +30,97 @@ class rtw_mrp_production_revised_edition(models.Model):
     def _compute_display_name(self):
         for record in self:
             # record.display_name = record.name
+            order_no = ''
             product_no = ''
             date_planned = ''
-            sale_order_no = ''
+            child_MO = None
             if record.sale_reference:
-                sale_order = self.env['sale.order'].search([ #GET SALE_ORDER By order_id
-                    ('name','=', record.sale_reference)
-                ], limit=1)
-                if record.product_id.product_no:
-                    product_no = f'/{record.product_id.product_no}'
-                if sale_order:
-                    if sale_order.estimated_shipping_date:
-                        date_planned = '/' + str(sale_order.estimated_shipping_date)
-                    record.display_name = f'{record.sale_reference}{product_no}{date_planned}'
-            else:
-                if record.product_id.product_no:
-                    product_no = f'/{record.product_id.product_no}'
-                record.display_name = f'{record.name}{product_no}'
+                order_no = record.sale_reference
+                child_MO = self.env["mrp.production"].search([('sale_reference', '=', order_no), ('origin', '=', record.name)])
+                
+            if record.product_id and record.product_id.product_no:
+                product_no = record.product_id.product_no
+            if record.estimated_shipping_date:
+                date_planned = str(record.estimated_shipping_date)
+            
+            display_name = ''
+            if record.is_drag_drop_calendar:
+                display_name = '[✔] '
+                
+            if order_no:
+                if product_no and date_planned:
+                    display_name += f'{order_no}/{product_no}/{date_planned}'
+                elif product_no:
+                    display_name += f'{order_no}/{product_no}'
+                elif date_planned:
+                    display_name += f'{order_no}/{date_planned}'
+                else:
+                    display_name += order_no
+            elif product_no:
+                if date_planned:
+                    display_name += f'{product_no}/{date_planned}'
+                else:
+                    display_name += product_no
+            elif date_planned:
+                display_name += date_planned
+                
+            record.display_name = display_name
+            
+            if child_MO:
+                for child in child_MO:
+                    arrival_schedule = ''
+                    if child.date_planned_start:
+                        arrival_schedule += str(child.date_planned_start) + "\n"
+                        
+                    record.prod_parts_arrival_schedule = arrival_schedule.rstrip('\n')
+                
+    def write(self, vals):
+        old_date_planned_start = {record.id: record.date_planned_start for record in self}
 
-    def write(self,vals):
-        result = super(rtw_mrp_production_revised_edition, self).write(vals)
-        if self.sale_reference:
-            sale_order = self.env['sale.order'].search([('name','=', self.sale_reference)])
-            if sale_order and 'estimated_shipping_date' in vals:
-                sale_order.estimated_shipping_date = vals['estimated_shipping_date']
-        return result
+        res = super(MrpProductionCus, self).write(vals)
+        for record in self:
+            if record.sale_reference and 'estimated_shipping_date' in vals:
+                so = self.env['sale.order'].search([('name', '=', record.sale_reference)])
+                so.estimated_shipping_date = vals['estimated_shipping_date']
 
+            if 'date_planned_start' in vals:
+                old_date = old_date_planned_start.get(record.id)
+                new_date = vals['date_planned_start']
+
+                if isinstance(new_date, str):
+                    new_date = parse(new_date)
+
+                if new_date and old_date and new_date < old_date:
+                    po = self.env["purchase.order"].search([('origin', 'ilike', record.name)])
+                    if po:
+                        for p in po:
+                            p.check_schedule_boolean = True
+                            
+                    mo_child = self.env["mrp.production"].search([('mrp_reference', 'ilike', record.name)])
+                    if mo_child:
+                        for mo in mo_child:
+                            po_child = self.env["purchase.order"].search([('origin', 'ilike', mo.name)])
+                            if po_child:
+                                for po in po_child:
+                                    po.check_schedule_boolean = True
+        return res
+    
+    @api.model
+    def update_date_planned_start(self, record):
+        id = int(record.get('id'))
+        time_str = str(record.get('time'))
+        if id and time_str:
+            manu_oder = self.env["mrp.production"].search([('id', '=', id)])
+            if manu_oder:
+                for mo in manu_oder:
+                    mo.is_drag_drop_calendar = True
+                    convert_date = datetime.fromisoformat(time_str.replace('Z', '+00:00'))
+                    # Convert to UTC timezone
+                    utc_datetime = convert_date.astimezone(pytz.utc)
+                    naive_datetime = utc_datetime.replace(tzinfo=None)
+                    mo.date_planned_start = naive_datetime
+        return True
+            
 class SaleOrder(models.Model):
     _inherit = "sale.order"
     
@@ -61,7 +129,7 @@ class SaleOrder(models.Model):
         if self.estimated_shipping_date:
             mrp_production = self.env['mrp.production'].search(
                 [('sale_reference', '=', self.name)])
-            for mrp in mrp_production:
-                mrp.write({"estimated_shipping_date":self.estimated_shipping_date})
-
+            if mrp_production:
+                for mrp in mrp_production:
+                    mrp.write({"estimated_shipping_date":self.estimated_shipping_date})
         return result
