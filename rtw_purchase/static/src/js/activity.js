62887d1c2a02c4133de2b007f4d10b827befd5e9
Phu270298 <phunh@axalize.vn>
2024-07-04 11:35:10 +0700

    fb198 + show schedule_check field name

--- rtw_purchase/static/src/js/activity.js
+++ rtw_purchase/static/src/js/activity.js
@@ -1,17 +1,17 @@
 odoo.define('rtw_purchase.check_schedule_activity', function (require) {
+    'use strict';
 
     var AbstractField = require('web.AbstractField');
     var field_registry = require('web.field_registry');
     var core = require('web.core');
-    var _t = core._t;
+    const _lt = core._lt;
 
     var CheckScheduleActivity = AbstractField.extend({
-        noLabel: true,
         fieldDependencies: _.extend({}, AbstractField.prototype.fieldDependencies, {
             check_schedule_icon: { type: 'char' },
             check_schedule_boolean: { type: 'boolean' },
         }),
-
+        label: _lt('日程確認'),
         /**
          * There is no edit mode for this widget, the icon is always readonly.
          *
@@ -35,15 +35,8 @@ odoo.define('rtw_purchase.check_schedule_activity', function (require) {
             setTimeout(function () {
                 if (self.recordData.check_schedule_boolean) {
                     self.$el.removeClass('o_field_empty');
-                    self.$el.addClass("pull-right text-warning fa");
+                    self.$el.addClass("text-center text-warning fa fa-lg w-100");
                     self.$el.addClass(self.recordData.check_schedule_icon || '');
-                    self.$el.attr({
-                        title: _t('日程確認')
-                    });
-                    self.$el.tooltip({
-                        placement: 'top',
-                        container: 'body'
-                    });
                 }
             }, 0);
         }
