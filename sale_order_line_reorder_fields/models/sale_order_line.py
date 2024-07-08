1fa7fa00e54c77e8efcecc7d3a52aad27d9f4cba
Phu270298 <phunh@axalize.vn>
2024-07-08 09:54:23 +0700

    fb190

--- sale_order_line_reorder_fields/models/sale_order_line.py
+++ sale_order_line_reorder_fields/models/sale_order_line.py
@@ -4,23 +4,41 @@ class SaleOrderLineExtend(models.Model):
     _inherit = 'sale.order.line'
 
     call_rate = fields.Float('Call Rate')
+    updating_discount  = fields.Boolean()
+    updating_call_rate  = fields.Boolean()
     
     @api.onchange('discount')
-    def change_discount(self):
+    def _onchange_discount(self):
         for line in self:
             if line.discount:
                 line.call_rate = 100 - line.discount
-                
+                line.updating_discount = True
+
     @api.onchange('call_rate')
-    def change_call_rate(self):
+    def _onchange_call_rate(self):
         for line in self:
             if line.call_rate:
                 line.discount = 100 - line.call_rate
-                
-    @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
-    def _onchange_discount(self):
-        super(SaleOrderLineExtend, self)._onchange_discount()
-
-        if self.discount is not None:
-            self.call_rate = 100 - self.discount
+                line.updating_call_rate = True
+    
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        self.ensure_one()
+        if self.updating_discount or self.updating_call_rate:
+            return
+        elif self.order_id and self.order_id.pricelist_id and self.order_id.pricelist_id.discount_policy == 'without_discount' and self.order_id.pricelist_id.item_ids:
+            last_item = self.order_id.pricelist_id.item_ids[-1]
+            price_str = last_item.price
+            percent_value = float(price_str.split()[0].replace('%', ''))
             
+            self.discount = percent_value
+            self.call_rate = 100 - self.discount
+        
+    def write(self, vals):
+        res = super(SaleOrderLineExtend, self).write(vals)
+        for line in self:
+            if line.call_rate and line.discount:
+                break
+            elif line.discount:
+                line.call_rate = 100 - line.discount
+        return res
