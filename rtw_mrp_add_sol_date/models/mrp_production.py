7be5f7213223a14e692b9a09a98ab5f6509d7283
Phu270298 <phunh@axalize.vn>
2024-07-10 09:31:26 +0700

    task no134

--- rtw_mrp_add_sol_date/models/mrp_production.py
+++ rtw_mrp_add_sol_date/models/mrp_production.py
@@ -6,11 +6,12 @@ from odoo import models, fields, api
 class rtw_mrp_production_add_sol_date(models.Model):
     _inherit = 'mrp.production'
     
-    shiratani_date = fields.Date(string='白谷到着日',compute='_compute_sol_date')
-    depo_date = fields.Date(string='デポ到着日',compute='_compute_sol_date')
-    preferred_delivery_date = fields.Date(string='配達希望日',compute='_compute_sol_date')
+    shiratani_date = fields.Date(string='白谷到着日', compute='_compute_sol_date')
+    depo_date = fields.Date(string='デポ到着日', compute='_compute_sol_date')
+    preferred_delivery_date = fields.Date(string='配達希望日', compute='_compute_sol_date')
     estimated_shipping_date = fields.Date(string='発送予定日')
-    
+    itoshima_shipping_date = fields.Date(string="糸島出荷日", compute='_compute_itoshima_shipping_date')
+
     def _get_so_from_mrp(self , mrp_production , count = 0):
           if count >= 10:
               return False
@@ -44,3 +45,17 @@ class rtw_mrp_production_add_sol_date(models.Model):
                 record.depo_date = ''
                 record.preferred_delivery_date = ''
     
+    def _compute_itoshima_shipping_date(self):
+        for mo in self:
+            scheduled_date = ''
+            if mo.sale_reference:
+                so = self.env["sale.order"].search([('name', '=', mo.sale_reference)], limit=1)
+                if so:
+                    pickings = self.env["stock.picking"].search([('sale_id', '=', so.id)])
+                    for sp in pickings:
+                        if sp.scheduled_date:                            
+                            if not scheduled_date or scheduled_date > sp.scheduled_date:
+                                scheduled_date = sp.scheduled_date
+                                
+            mo.itoshima_shipping_date = scheduled_date          
+                
