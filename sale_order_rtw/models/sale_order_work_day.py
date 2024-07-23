cb7824ff9daf816bca8a336dfe4964d1d3067d7a
Phu270298 <phunh@axalize.vn>
2024-07-23 15:50:55 +0700

    fb212

--- sale_order_rtw/models/sale_order_work_day.py
+++ sale_order_rtw/models/sale_order_work_day.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 class sale_order_work_day(models.Model):
     _name = 'sale.order.work.day'
     _description = 'Selection Work Day'
-    _order = "id"
+    _order = "sequence"
     
     name = fields.Char('作成日数' , required=True)
+    sequence = fields.Integer(default=10)
