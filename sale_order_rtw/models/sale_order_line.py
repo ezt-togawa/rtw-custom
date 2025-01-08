ca4bf243b60c0e88b99cc42b703799cb9ac11a95
小川智丈 <t_ogawa@enzantrades.co.jp>
2025-01-08 20:25:14 +0900

    販売明細順確認2（TEST PRINT）

--- sale_order_rtw/models/sale_order_line.py
+++ sale_order_rtw/models/sale_order_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import logging
 from odoo import models, fields, api
 
 
@@ -32,21 +32,13 @@ class sale_order_line_rtw(models.Model):
         return res
 
     # 明細行の並び順担保処理（OCAのモジュール前提：sale_order_line_sequence）
-    def create(self, vals):
-        res = super(sale_order_line_rtw, self).create(vals)
-        # visible_sequence はOCAの項目、表示上の順番の番号。sequenceの初期値はOdoo側で9999が設定される
-        # 複数行追加後に並び順を変えると、Odoo側でsequenceの+1をして、10000以上の数値なり以降の追加行が間に入るので順番が狂うのを調整する
-        # メモやセクションは visible_sequence の対象外のようなので、display_type で判断して除外する
-        for line in res:
-            order = line.mapped("order_id")
-            if order:
-                # メモ/セクション数
-                filtered_lines = order.order_line.filtered(lambda l: l.sequence < 9999)
-                if filtered_lines:
-                    order_lines_seq_max = max(filtered_lines, key=lambda l: l.sequence).sequence
-                else:
-                    order_lines_seq_max = 0
-                if not line.display_type and line.sequence >= 9999:
-                    # 新規追加時は明細の9999を除く最大Sequenceにプラス1した値をセットする
-                    line.sequence = order_lines_seq_max + 1
+    def write(self, vals):
+        print('sale_order_rtw->write')
+        for record in self:
+            print('1:', record, record.sequence, record.visible_sequence)
+            # visible_sequence はOCAの項目、表示上の順番の番号、初期値9999
+            # 複数行追加後に並び順を変えると、Odoo側でsequenceの+1をして、10000以上の数値なり以降の追加行が間に入るので順番が狂うのを調整する
+            vals['sequence'] = record.visible_sequence
+            print('2:', record.sequence, record.visible_sequence)
+        res = super(sale_order_line_rtw, self).write(vals)
         return res
