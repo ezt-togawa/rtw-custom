bce7e2ecde7f0714899238e3a3b656b34c476271
小川智丈 <t_ogawa@enzantrades.co.jp>
2025-01-08 22:53:41 +0900

    販売明細順確認3（TEST PRINT）

--- sale_order_rtw/models/sale_order_line.py
+++ sale_order_rtw/models/sale_order_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import logging
+
 from odoo import models, fields, api
 
 
@@ -33,12 +33,22 @@ class sale_order_line_rtw(models.Model):
 
     # 明細行の並び順担保処理（OCAのモジュール前提：sale_order_line_sequence）
     def write(self, vals):
-        print('sale_order_rtw->write')
-        for record in self:
-            print('1:', record, record.sequence, record.visible_sequence)
+        print('vals：', vals)
+        print('sale_order_rtw->write', self.name, self.sequence, self.visible_sequence)
+        before_seq = self.sequence
+
+        print('1 更新前後SEQ:', self.name, before_seq, self.sequence)
+        print('2 更新結果SEQ/VIS:', self.name, self.sequence, self.visible_sequence)
+        # for record in self:
+        #     print('1:', record, record.sequence, record.visible_sequence)
             # visible_sequence はOCAの項目、表示上の順番の番号、初期値9999
             # 複数行追加後に並び順を変えると、Odoo側でsequenceの+1をして、10000以上の数値なり以降の追加行が間に入るので順番が狂うのを調整する
-            vals['sequence'] = record.visible_sequence
-            print('2:', record.sequence, record.visible_sequence)
-        res = super(sale_order_line_rtw, self).write(vals)
-        return res
+            # vals['sequence'] = record.visible_sequence
+            # record.sequence = record.visible_sequence
+            # print('2:', record.sequence, record.visible_sequence)
+        # return res
+        if 'visible_sequence' in vals:
+            vals['sequence'] = vals['visible_sequence']
+            # self.sequence = self.visible_sequence
+            print('3 SEQ変更:', self.name, before_seq, self.visible_sequence)
+        return super(sale_order_line_rtw, self).write(vals)
