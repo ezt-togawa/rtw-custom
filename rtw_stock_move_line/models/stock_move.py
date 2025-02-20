603332e0d132db80c5c20d59b24e7af0b416ad6e
Dangdhh <khanhvc@axalize.vn>
2025-02-20 15:03:20 +0700

    fb 257 258

--- rtw_stock_move_line/models/stock_move.py
+++ rtw_stock_move_line/models/stock_move.py
@@ -20,8 +20,8 @@ class rtw_stock_move(models.Model):
         related="sale_id.overseas", string="海外")
     factory = fields.Many2one(related="production_id.picking_type_id")
     memo = fields.Char(related='sale_line_id.memo')
-    area = fields.Many2one('res.country.state', compute="_get_area", string='エリア', store=True)
-    area_2 = fields.Many2one('res.country.state', compute="_get_area_2", store=True)
+    area = fields.Text( compute="_get_area", string='エリア', store=True)
+    area_2 = fields.Text( compute="_get_area_2", store=True)
     forwarding_address = fields.Text(
         compute="_get_forwarding_address", string='到着地',store=True)
     shipping_to = fields.Text(
@@ -216,29 +216,30 @@ class rtw_stock_move(models.Model):
         else:
             rec.shipping_to = False
 
-    @api.depends('sale_id','sale_id','picking_id.waypoint', 'sale_id.waypoint', 'picking_id.waypoint.state_id', 'sale_id.waypoint.state_id')
+    @api.depends('sale_id','sale_id','picking_id.waypoint', 'sale_id.waypoint', 'picking_id.waypoint.display_name', 'sale_id.waypoint.display_name')
     def _get_area(self):
         for rec in self:
             if rec.picking_id and rec.picking_id.waypoint:
-                rec.area = rec.picking_id.waypoint.state_id
-                if rec.sale_id and not rec.picking_id.waypoint.state_id:
-                    rec.area = rec.sale_id.waypoint.state_id
+                rec.area = rec.picking_id.waypoint.display_name
+                if rec.sale_id and not rec.picking_id.waypoint:
+                    rec.area = rec.sale_id.waypoint.display_name
             elif rec.sale_id and rec.sale_id.waypoint:
-                rec.area = rec.sale_id.waypoint.state_id
+                rec.area = rec.sale_id.waypoint.display_name
             else:
                 rec.area = False
 
-    @api.depends('sale_id','sale_id','picking_id.waypoint_2', 'sale_id.waypoint_2', 'picking_id.waypoint_2.state_id', 'sale_id.waypoint_2.state_id')
+    @api.depends('sale_id','sale_id','picking_id.waypoint_2', 'sale_id.waypoint_2', 'picking_id.waypoint_2.display_name', 'sale_id.waypoint_2.display_name')
     def _get_area_2(self):
         for rec in self:
             if rec.picking_id and rec.picking_id.waypoint_2:
-                rec.area_2 = rec.picking_id.waypoint_2.state_id
-                if rec.sale_id and not rec.picking_id.waypoint_2.state_id:
-                    rec.area_2 = rec.sale_id.waypoint_2.state_id
+                rec.area_2 = rec.picking_id.waypoint_2.display_name
+                if rec.sale_id and not rec.picking_id.waypoint_2:
+                    rec.area_2 = rec.sale_id.waypoint_2.display_name
             elif rec.sale_id and rec.sale_id.waypoint_2:
-                rec.area_2 = rec.sale_id.waypoint_2.state_id
+                rec.area_2 = rec.sale_id.waypoint_2.display_name
             else:
                 rec.area_2 = False
+            
                 
     def _get_primary_shipment_stock_move(self):
         for move in self:
