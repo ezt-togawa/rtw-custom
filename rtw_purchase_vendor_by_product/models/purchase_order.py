43142dfc3330100d8cf7136a0a2f8409311058c6
Dangdhh <khanhvc@axalize.vn>
2025-02-20 16:37:47 +0700

    fb 255

--- rtw_purchase_vendor_by_product/models/purchase_order.py
+++ rtw_purchase_vendor_by_product/models/purchase_order.py
@@ -10,21 +10,7 @@ class rtw_purchase(models.Model):
         string='Order Line Exists',
         compute='_compute_order_line_exists',
         store=True
-    )
-    partner_id = fields.Many2one('res.partner',  domain="[('id', 'in', partner_ids)]")
-    partner_ids = fields.Many2many('res.partner',compute ='compute_partner_ids')
-
-    def compute_partner_ids(self):
-        partner_ids = []
-        for line in self:
-            if line.order_line:
-                for order in line.order_line:
-                    partner_ids.extend(order.product_id.seller_ids.mapped('name.id'))
-                line.partner_ids =tuple(partner_ids)
-            else:
-                partner_ids = self.env['res.partner'].search([]).ids
-                line.partner_ids = partner_ids
-      
+    ) 
     @api.depends('order_line')
     def _compute_order_line_exists(self):
         for order in self: 
@@ -33,11 +19,9 @@ class rtw_purchase(models.Model):
     @api.onchange('order_line')
     def onchange_order_line(self):
         supplier_id = False
-        partner_ids = []
         for line in self:
             if line.order_line:
                 for order in line.order_line:
-                    partner_ids = order.product_id.seller_ids.mapped('name.id')
                     if order.product_id.seller_ids:
                         if not supplier_id:
                             supplier_id = order.product_id.seller_ids[0].name.id
@@ -51,7 +35,7 @@ class rtw_purchase(models.Model):
                 if self.partner_id and self.currency_id:
                     for line in self.order_line:
                         line.onchange_product_id()
-            return {'domain': {'partner_id': [('id', 'in', partner_ids), '|', ('company_id', '=', False), ('company_id', '=', line.company_id.id)]}}
+            # return {'domain': {'partner_id': [('id', '=', self.partner_id.id), '|', ('company_id', '=', False), ('company_id', '=', line.company_id.id)]}}
         else:
             return {'domain': {'partner_id': ['|', ('company_id', '=', False), ('company_id', '=', line.company_id.id)]}}
 
