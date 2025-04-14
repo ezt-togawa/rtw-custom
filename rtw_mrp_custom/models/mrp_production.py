e1314825f18f936c5e5ec57336c6554a4784dd9b
truongdo1809 <140844361+truongdo1809@users.noreply.github.com>
2025-04-14 14:22:00 +0700

    no 181

--- rtw_mrp_custom/models/mrp_production.py
+++ rtw_mrp_custom/models/mrp_production.py
@@ -95,7 +95,7 @@ class MrpProductionCus(models.Model):
             else:
                 sale_order = self.env['sale.order'].search([('name', '=', record.sale_reference)], limit=1)
                 warehouse_name = record.picking_type_id.warehouse_id.name
-                if warehouse_name == "糸島工場":
+                if warehouse_name in ["糸島工場","日東木工","アサヒ", "酒見椅子"]:
                     if sale_order.sipping_to:
                         if sale_order.sipping_to == 'direct':
                             record.address_ship = '直送' 
