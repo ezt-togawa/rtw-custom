51b81b63a14e63444e0a5181de9a37ec0e78bb67
小川智丈 <t_ogawa@enzantrades.co.jp>
2025-07-07 13:37:55 +0900

    No211,発注状況の作業オーダー一覧への表出＆ViewのID重複修正

--- rtw_update_mrp_workorder/models/mrp_workorder.py
+++ rtw_update_mrp_workorder/models/mrp_workorder.py
@@ -5,3 +5,11 @@ class AccountInvoice(models.Model):
     _inherit = "mrp.workorder"
     
     implementation_plan_date = fields.Date('Implementation Plan Date')
+    mrp_workorder_order_status = fields.Boolean('Mrp Order Status', compute="_compute_workorder_status")
+
+    def _compute_workorder_status(self):
+        for record in self:
+            if record.production_id:
+                record.mrp_workorder_order_status = record.production_id.mrp_order_status
+            else:
+                record.mrp_workorder_order_status = False
