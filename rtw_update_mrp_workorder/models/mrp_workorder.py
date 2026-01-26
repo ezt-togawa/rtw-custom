1f3f8a18ad58ae191ccd987d4f3a907c74cac690
truongdo1809 <140844361+truongdo1809@users.noreply.github.com>
2026-01-26 15:50:28 +0700

    no 246

--- rtw_update_mrp_workorder/models/mrp_workorder.py
+++ rtw_update_mrp_workorder/models/mrp_workorder.py
@@ -6,6 +6,10 @@ class AccountInvoice(models.Model):
     
     implementation_plan_date = fields.Date('Implementation Plan Date')
     mrp_workorder_order_status = fields.Boolean('Mrp Order Status', compute="_compute_workorder_status")
+    planned_production_start_date = fields.Date('Planned Production Start Date',compute="_compute_planned_production_start_date", store=True,)
+    quantity = fields.Float('Quantity', compute='_compute_quantity')
+    work_notes = fields.Char('作業メモ')
+
 
     def _compute_workorder_status(self):
         for record in self:
@@ -13,3 +17,17 @@ class AccountInvoice(models.Model):
                 record.mrp_workorder_order_status = record.production_id.mrp_order_status
             else:
                 record.mrp_workorder_order_status = False
+
+    def _compute_planned_production_start_date(self):
+        for record in self:
+            if record.production_id:
+                record.planned_production_start_date = record.production_id.date_planned_start
+            else:
+                record.planned_production_start_date = False
+
+    def _compute_quantity(self):
+        for record in self:
+            if record.production_id:
+                record.quantity = record.production_id.product_qty
+            else:
+                record.quantity = 0.0
