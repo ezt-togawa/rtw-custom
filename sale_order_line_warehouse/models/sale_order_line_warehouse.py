552c0a49159f2840b525dd2a95ddbeab3db33a48
小川智丈 <t_ogawa@enzantrades.co.jp>
2026-02-26 18:44:19 +0900

    販売明細の倉庫ロジックのスリム化、画面表示の不安定さに起因しそうなので一旦対処してみる（倉庫が設定外の倉庫が表示される場合があるというタイミングエラー的な状況の対策）

--- sale_order_line_warehouse/models/sale_order_line_warehouse.py
+++ sale_order_line_warehouse/models/sale_order_line_warehouse.py
@@ -6,66 +6,71 @@ class sale_order_line_warehouse(models.Model):
      _inherit = "sale.order.line"
 
      warehouse_id = fields.Many2one(
-         'stock.warehouse' , store=True , domain= "[('id' , 'in' , allowed_warehouse_ids)]"
+         'stock.warehouse', store=True, domain="[('id' , 'in' , allowed_warehouse_ids)]"
      )
 
-     allowed_warehouse_ids = fields.Many2many( # THIS FIELD STORES THE APPROPRIATE WAREHOUSES
-        'stock.warehouse', compute='_compute_allowed_warehouses'
+     allowed_warehouse_ids = fields.Many2many(  # THIS FIELD STORES THE APPROPRIATE WAREHOUSES
+        'stock.warehouse', compute='_compute_allowed_warehouses', store=False
      )
      
      @api.onchange('warehouse_id')
      def onchange_warehouse_id(self):
         for line in self:
-            product = self.env['product.product'].search([('id' , '=' , line.product_id.id)])
-            bom_ids = self.env['mrp.bom'].search([('product_tmpl_id', '=' , product.product_tmpl_id.id)])
+            product_tmpl = line.product_id.product_tmpl_id
+            if not product_tmpl or not line.warehouse_id:
+                continue
+
             result_bom_id = False
-            for bom in bom_ids:
+            for bom in product_tmpl.bom_ids:
                 if line.warehouse_id == bom.picking_type_id.warehouse_id:
                     result_bom_id = bom
                     break
             if result_bom_id:
                 line.bom_id = result_bom_id
 
-     @api.depends('product_id','warehouse_id')
+     @api.depends('product_id')
      def _compute_allowed_warehouses(self):
         for line in self:
-            product = self.env['product.product'].search([('id' , '=' , line.product_id.id)])
+            if not line.product_id:
+                line.allowed_warehouse_ids = [(5, 0, 0)]  # (5,0,0)は「全クリア」の意味
+                continue
+
             warehouses = []
-            if product:
-                for route in product.route_ids:
-                    for rule in route.rule_ids:
-                        if rule.warehouse_id.id not in warehouses and rule.warehouse_id:
-                            warehouses.append(rule.warehouse_id.id)
-            line.allowed_warehouse_ids = warehouses
-            if not line.warehouse_id: #SET DEFAULT WAREHOUSE
-               if line.allowed_warehouse_ids:
-                    line.warehouse_id = line.allowed_warehouse_ids[0]
-               else:
-                    line.warehouse_id = None
+            for route in line.product_id.route_ids:
+                for rule in route.rule_ids:
+                    if rule.warehouse_id.id not in warehouses and rule.warehouse_id:
+                        warehouses.append(rule.warehouse_id.id)
+            line.allowed_warehouse_ids = [(6, 0, warehouses)]   # (6, 0)は入れ替えの意味
+            if not line.warehouse_id and warehouses:
+                line.warehouse_id = warehouses[0]
 
-     def _prepare_add_missing_fields(self , values): # SET DEFAULT WAREHOUSE WHEN CREATE SALE ORDER LINE WITH CONFIGURE PRODUCT
+     def _prepare_add_missing_fields(self, values): # SET DEFAULT WAREHOUSE WHEN CREATE SALE ORDER LINE WITH CONFIGURE PRODUCT
         res = super(sale_order_line_warehouse, self)._prepare_add_missing_fields(values)
-        warehouses = []
-        product = self.env['product.product'].search([('id' , '=' , values['product_id'])])
-        bom_ids = self.env['mrp.bom'].search([('product_tmpl_id', '=' , product.product_tmpl_id.id)])
-        result_bom_id = False
-        
-        for route in product.route_ids:
-                  for rule in route.rule_ids:
-                      if rule.warehouse_id.id not in warehouses and rule.warehouse_id:
-                          warehouses.append(rule.warehouse_id.id)
-                          
-        if warehouses:
-             res['warehouse_id'] = warehouses[0]
-        else:
-             res['warehouse_id'] = False
+        if 'product_id' not in values or not values['product_id']:
+            return res
 
-        for bom in bom_ids:
-            if res['warehouse_id'] == bom.picking_type_id.warehouse_id.id:
-                  result_bom_id = bom.id
-                  break
-        if result_bom_id:
-            res['bom_id'] = result_bom_id
+        product = self.env['product.product'].browse(values['product_id'])
+        if not product.exists():
+            return res
+
+        warehouse_ids = product.route_ids.mapped('rule_ids.warehouse_id').ids
+        selected_wh_id = warehouse_ids[0] if warehouse_ids else False
+        res['warehouse_id'] = selected_wh_id
+
+        if selected_wh_id:
+            # product.bom_ids は直接テンプレートに紐づくBOM
+            bom_ids = product.bom_ids or product.product_tmpl_id.bom_ids
+
+            result_bom_id = False
+            for bom in bom_ids:
+                # 倉庫ID同士で比較
+                if selected_wh_id == bom.picking_type_id.warehouse_id.id:
+                    result_bom_id = bom.id
+                    break
+
+            if result_bom_id:
+                res['bom_id'] = result_bom_id
+
         return res
 
 
