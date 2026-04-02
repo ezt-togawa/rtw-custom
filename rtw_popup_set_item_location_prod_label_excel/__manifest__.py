3e2953bed895f6436c99ea6828f9a2591b5a703c
truongdo1809 <140844361+truongdo1809@users.noreply.github.com>
2026-04-02 14:53:09 +0700

    no 251

--- rtw_popup_set_item_location_prod_label_excel/__manifest__.py
+++ rtw_popup_set_item_location_prod_label_excel/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mrp', 'stock', 'rtw_stock_move_line'],
+    'depends': ['base', 'mrp', 'stock', 'rtw_stock_move_line','rtw_mrp_custom'],
 
     # always loaded
     'data': [
