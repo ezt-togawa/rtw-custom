e40426afac5ded7aeef5337d19548f0422fb6f72
小川智丈 <t_ogawa@enzantrades.co.jp>
2026-06-22 16:09:58 +0900

    パレットのステータスにPiking（運送）の有無を考慮

--- stock_move_container/models/stock_move_pallet.py
+++ stock_move_container/models/stock_move_pallet.py
@@ -7,12 +7,15 @@ class StockMovePallet(models.Model):
    
     container_id = fields.Many2one('stock.move.container', 'ContainerId')
 
-    status = fields.Char('ステータス', compute="_compute_status", store=True,default="配送待")
+    status = fields.Char('ステータス', compute="_compute_status", store=True, default="配送待")
 
-    @api.depends('move_ids.state')
+    @api.depends('move_ids.state', 'move_ids.picking_id')
     def _compute_status(self):
         for record in self:
-            if all(line.state == 'done' for line in record.move_ids):
+            # 運送(Piking)に紐づく配送(move)のみ対象にしてステータスチェックをする
+            picking_moves = record.move_ids.filtered(lambda m: m.picking_id)
+
+            if picking_moves and all(line.state == 'done' for line in picking_moves):
                 status = "配送完了"
             else:
                 status = "配送待"  
